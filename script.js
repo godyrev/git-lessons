6dad35463544dab63287333e75875f4dcb64d51a
GSS <godyrev@gmail.com>
2021-02-04 14:32:25 +0300

    Мнение об уроке

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const p = document.createElement('p')
+let p = document.createElement('p')
 p.innerText = 'something text content'
 document.body.appendChild(p)
+
+const text = "Пиздец какая нудятина.";
+
+let count = 0;
+
+p = document.createElement('p')
+p.classList.add('pizdec')
+document.body.appendChild(p)
+
+const i = setInterval(() => {
+    p.textContent += text[count];
+    count++;
+    if(count >= text.length){
+        clearInterval(i)
+    }
+}, 100)
